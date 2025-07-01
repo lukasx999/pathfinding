78531865406016350bf5693968e32757865ad1c5
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-07-01 19:01:35 +0200

    refactoring

--- main.cc
+++ main.cc
@@ -51,10 +51,7 @@ class Solver {
         Terminated,
     } m_state = State::Idle;
 
-    // rendering
-    static constexpr float m_fontsize = 50;
-    static constexpr Vector2 m_draw_offset { WIDTH/2.0f, HEIGHT/2.0f };
-    static constexpr float m_vertex_spacing = 300;
+    friend class Renderer;
 
 public:
     Solver(std::unordered_map<VertexId, Vertex> vertices, VertexId source)
@@ -69,40 +66,7 @@ public:
 
     }
 
-    void draw() const {
-
-        DrawText(std::format("unvisited: {}", m_unvisited).c_str(), 0, 0, m_fontsize, WHITE);
-        DrawText(std::format("state: {}", stringify_state(m_state)).c_str(), 0, m_fontsize, m_fontsize, WHITE);
-
-        draw_distance_table({ 0, m_fontsize*3 });
-
-        for (auto &[key, value] : m_vertices) {
-            auto pos = value.m_pos * m_vertex_spacing + m_draw_offset;
-            draw_neighbours(pos, value.m_neighbours, key);
-        }
-
-        for (auto &[key, value] : m_vertices) {
-            // TODO: remove code duplication
-            auto pos = value.m_pos * m_vertex_spacing + m_draw_offset;
-            auto color = value.m_id == m_current ? RED : BLUE;
-
-            if (m_state == State::Visiting) {
-                auto neighbour = m_neighbour->m_other_id;
-                if (value.m_id == neighbour)
-                    color = GREEN;
-            }
-
-            float radius = 30;
-            DrawCircleV(value.m_pos * m_vertex_spacing + m_draw_offset, radius, color);
-
-            float fontsize = 50;
-            draw_text_centered(std::format("{}", key), pos, fontsize, WHITE);
-
-        }
-    }
-
     void next() {
-
         switch (m_state) {
             case State::Terminated:
                 ;
@@ -146,48 +110,6 @@ public:
     }
 
 private:
-    void draw_neighbours(Vector2 vertex_pos, std::span<const Edge> edges, VertexId source) const {
-
-        for (const auto &edge : edges) {
-
-            VertexId id = edge.m_other_id;
-            auto vtx = m_vertices.at(id);
-            auto pos = vtx.m_pos * m_vertex_spacing + m_draw_offset;
-
-            auto color = GRAY;
-
-            if (m_state == State::Visiting) {
-                if (source == m_current && m_neighbour->m_other_id == id) {
-                    color = GREEN;
-                }
-            }
-
-            DrawLineEx(vertex_pos, pos, 5, color);
-
-            auto diff = pos - vertex_pos;
-            float dist = Vector2Length(diff) / 2.0f;
-            auto line_middle = vertex_pos + Vector2Normalize(diff) * dist;
-
-            float fontsize = 50;
-            draw_text_centered(std::format("{}", edge.m_weight), line_middle, fontsize, WHITE);
-
-        }
-
-    }
-
-    void draw_distance_table(Vector2 pos) const {
-        for (auto &&[idx, kv] : std::views::enumerate(m_distances)) {
-            auto &[key, dist] = kv;
-            DrawText(
-                std::format("{}: {}", key, dist).c_str(),
-                pos.x,
-                m_fontsize*idx + pos.y,
-                m_fontsize,
-                WHITE
-            );
-        }
-    }
-
     inline void next_unvisited() {
         m_current = *ranges::min_element(m_unvisited, [&](VertexId a, VertexId b) {
             return m_distances.at(a) < m_distances.at(b);
@@ -223,6 +145,104 @@ private:
 
 };
 
+class Renderer {
+    const Solver &m_solver;
+    static constexpr float m_fontsize = 50;
+    static constexpr Vector2 m_draw_offset { WIDTH/2.0f, HEIGHT/2.0f };
+    static constexpr float m_vertex_spacing = 300;
+
+public:
+    Renderer(const Solver &solver) : m_solver(solver) { }
+
+    void draw() const {
+
+        for (auto &[key, value] : m_solver.m_vertices) {
+            auto pos = value.m_pos * m_vertex_spacing + m_draw_offset;
+            draw_neighbours(pos, value.m_neighbours, key);
+        }
+
+        for (auto &[key, value] : m_solver.m_vertices) {
+            // TODO: remove code duplication
+            auto pos = value.m_pos * m_vertex_spacing + m_draw_offset;
+            auto color = value.m_id == m_solver.m_current ? RED : BLUE;
+
+            if (m_solver.m_state == Solver::State::Visiting) {
+                auto neighbour = m_solver.m_neighbour->m_other_id;
+                if (value.m_id == neighbour)
+                    color = GREEN;
+            }
+
+            float radius = 30;
+            DrawCircleV(value.m_pos * m_vertex_spacing + m_draw_offset, radius, color);
+
+            float fontsize = 50;
+            draw_text_centered(std::format("{}", key), pos, fontsize, WHITE);
+
+        }
+
+        draw_ui();
+
+    }
+
+private:
+    void draw_ui() const {
+        DrawText(std::format("unvisited: {}", m_solver.m_unvisited).c_str(), 0, 0, m_fontsize, WHITE);
+        DrawText(std::format("state: {}", m_solver.stringify_state(m_solver.m_state)).c_str(), 0, m_fontsize, m_fontsize, WHITE);
+
+        draw_distance_table({ 0, m_fontsize*3 });
+    }
+
+    void draw_distance_table(Vector2 pos) const {
+        for (auto &&[idx, kv] : std::views::enumerate(m_solver.m_distances)) {
+            auto &[key, dist] = kv;
+
+            DrawText(
+                std::format("{}: {}", key, dist).c_str(),
+                pos.x,
+                m_fontsize * idx + pos.y,
+                m_fontsize,
+                WHITE
+            );
+
+        }
+    }
+
+    void draw_neighbours(Vector2 vertex_pos, std::span<const Edge> edges, VertexId origin) const {
+
+        for (const auto &edge : edges) {
+
+            VertexId id = edge.m_other_id;
+            auto vtx = m_solver.m_vertices.at(id);
+            auto pos = vtx.m_pos * m_vertex_spacing + m_draw_offset;
+
+            auto color = GRAY;
+
+            if (m_solver.m_state == Solver::State::Visiting) {
+
+                bool is_origin = origin == m_solver.m_current;
+                bool is_currently_visited = m_solver.m_neighbour->m_other_id == id;
+
+                if (is_origin && is_currently_visited) {
+                    color = GREEN;
+                }
+
+            }
+
+            DrawLineEx(vertex_pos, pos, 5, color);
+
+            auto diff = pos - vertex_pos;
+            float dist = Vector2Length(diff) / 2.0f;
+            auto line_middle = vertex_pos + Vector2Normalize(diff) * dist;
+
+            float fontsize = 50;
+            draw_text_centered(std::format("{}", edge.m_weight), line_middle, fontsize, WHITE);
+
+        }
+
+    }
+
+};
+
 int main() {
 
     std::unordered_map<VertexId, Vertex> m_vertices {
@@ -234,6 +254,7 @@ int main() {
     };
 
     Solver solver(m_vertices, 1);
+    Renderer renderer(solver);
 
     InitWindow(WIDTH, HEIGHT, "Path Finding");
 
@@ -241,7 +262,7 @@ int main() {
         BeginDrawing();
         {
             ClearBackground(BLACK);
-            solver.draw();
+            renderer.draw();
 
             if (IsKeyPressed(KEY_J))
                 solver.next();
